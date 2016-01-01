017fe6ced2b926e6f0b5554238edfa39f550aecd
Mike <mlawson1986@gmail.com>
2016-01-01 12:56:57 -0600

    inital commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'lamp-chef'
+maintainer       'N/A'
+maintainer_email 'mlawson1986@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures lamp-chef'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
